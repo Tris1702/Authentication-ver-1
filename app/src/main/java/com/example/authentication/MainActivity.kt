5686ee10d24e77000d3c68b395d1b1c44c67a3c2
Tris1702 <phuonghoand2001@gmail.com>
2021-10-02 15:11:19 +0700

    Oct/2/2021

--- app/src/main/java/com/example/authentication/MainActivity.kt
+++ app/src/main/java/com/example/authentication/MainActivity.kt
@@ -6,36 +6,36 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Call truoc de khong co hien tuong nhay
+        doAfter(supportFragmentManager, applicationContext)
         val splashDialog = SplashDialog(this)
         splashDialog.startSplashDialog()
         lifecycleScope.launch{
             delay(3000)
             splashDialog.closeSplashDialog()
-            doAfter(supportFragmentManager, applicationContext)
         }
     }
 }
 
-fun doAfter( supportFragmentManager: FragmentManager, context: Context){
+fun doAfter(supportFragmentManager: FragmentManager, context: Context){
     val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
     val navController = navHostFragment.navController
-
     val sharesPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE)
     val token = sharesPreferences?.getString("accessToken", "none")
     if (token.equals("none")) //neu may khong luu token thi chuyen den signIn
     {
         navController.navigate(R.id.signIn)
-    }
-    else {
+    } else {
         //neu co thi chuyen den logout
-        navController.navigate(R.id.logOut)
+            navController.navigate(R.id.logOut)
     }
 }
